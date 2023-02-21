97ff7b78b28c813b6e97a0f0f952cf021ba6a531
Ethan Storm <stormethan10@gmail.com>
2023-02-21 12:28:12 -0800

    adds working test of "calling the weather api"

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
 const express = require('express')
 const app = express()
-const port = 8000
-
 // make dotenv work
 require('dotenv').config()
+const port = process.env.port // get port from .env
+// require file stream to read and update files
+const fs = require('fs')
+// include the weather data to read
+const weatherData = require('./data/weather.json')
 
 app.get('/', (req, res) => {
 	console.log('home route accessed')
 	res.send('Welcome')
+})
+
+app.get('/weather', (req, res) => {
+	console.log('getting weather')
+
+	const lat = req.query.lat
+	const lon = req.query.lon
+	const searchQuery = req.query.searchQuery
+
+	let place = weatherData.filter(location => {
+		if (
+			location.lat === lat &&
+			location.city_name === searchQuery &&
+			location.lon === lon
+		) {
+			return location
+		}
+	})
+
+	place
+		? res.status(200).send(place)
+		: res.status(404).send('location not found')
+})
+
+app.use('*', (req, res) => {
+	console.log(err)
+	res.status(404).send('That page is not found! ):')
 })
 
 app.listen(port, (req, res) => {
