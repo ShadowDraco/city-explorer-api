a512cd52cd8e1f1f0d2ebe339923ec311a3b4e84
Ethan Storm <stormethan10@gmail.com>
2023-02-23 10:02:07 -0800

    added weekday to date

--- Forecast.js
+++ Forecast.js
@@ -1,5 +1,7 @@
 class Forecast {
 	constructor(forecast) {
+		
+		let dateDay = new Date(forecast.datetime).getDay
 		this.date = forecast.datetime
 
 		let descriptionTemplate = `Temperature of ${forecast.temp}, feels ${forecast.app_temp} with ${forecast.weather.description}`
