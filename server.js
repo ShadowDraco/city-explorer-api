f53590c6e47d2175694f8f327da80b08be16a4f8
Ethan Storm <stormethan10@gmail.com>
2023-02-22 17:34:23 -0800

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT // get port from .env
 const cors = require('cors')
 
 const corsOptions = {
-	origin: 'http://localhost:3000',
+	origin: '*',
 
 	credentials: true, //access-control-allow-credentials:true
 	optionSuccessStatus: 200,
